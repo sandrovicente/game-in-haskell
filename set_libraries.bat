256e766ddeb21f21abdd3a74267f09a62597463b
SandroAV <sandrovicente@gmail.com>
2018-11-27 22:32:03 +0000

    fix to work on windows

---
+++ set_libraries.bat
@@ -0,0 +1,3 @@
+Set C_INCLUDE_PATH=C:\tools\openal-soft-1.19.1-bin\include;c:\tools\alut;C:\tools\mingw\freeglut\include
+Set LIBRARY_PATH=C:\tools\openal-soft-1.19.1-bin\libs\Win64\;c:\tools\alut;C:\tools\mingw\freeglut\lib\x64
+set PATH=%PATH%;C:\tools\mingw\freeglut\bin\x64
